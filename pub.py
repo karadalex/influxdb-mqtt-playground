7108bc7a3a1a17b1ecf3029064abb1465423e764
karadalex <karadalex@gmail.com>
2019-07-31 13:36:48 +0300

    format message in json

--- pub.py
+++ pub.py
@@ -1,6 +1,8 @@
 import paho.mqtt.client as mqtt
 import random
 import time
+import json
+import pprint
 
 
 broker = "localhost"
@@ -14,14 +16,21 @@ def on_publish(client, userdata, result):
 # create client object and connect
 client = mqtt.Client()
 client.username_pw_set("rabbitmq", password='rabbitmq')
-client.connect(broker,port)
+client.connect(broker, port, 45)
 
 # assign publish callback function
 client.on_publish = on_publish
 
 # publish messages
-price = 9500
+price = 9500.50
 while True:
     priceChange = random.randint(-50, 50)
-    ret = client.publish("crypto/btc", price + priceChange)
+    price = price + priceChange
+    dict_msg = {
+        "symbol": "BTC",
+        "price": price,
+    }
+    msg = json.dumps(dict_msg)
+    ret = client.publish("crypto/btc", msg)
+    print(msg)
     time.sleep(5)
